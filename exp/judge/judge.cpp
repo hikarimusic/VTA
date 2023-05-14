c8f26d395228934619452bb1047a8aa713e35a91
hikari <hikarimusic2002@gmail.com>
2023-05-14 13:37:54 +0800

    judge com

--- exp/judge/judge.cpp
+++ exp/judge/judge.cpp
@@ -81,12 +81,8 @@ void judge(char** argv) {
         if (line[1][0][0]!='@')
             break;
     }
-    // std::cout << line[0][9] << '\n' << line[1][9] << '\n';
     do {
         qn += 1;
-        // std::cout << line[0][9] << '\n' << line[1][9] << '\n';
-        // std::cout << cig_tp[0] << ' ' << cig_tp[1] << '\n';
-        // std::cout << line[1][1] << ' ' << ((std::stoi(lin[1][1])&128)/128) << '\n';
         while ((qry_tp==line[1][0]) && (cig_tp[(std::stoi(line[1][1])&128)/128])) {
             std::cout << '\r' << "    E. Duplicated: " << line[1][0] << "                    \n" << std::flush;
             parse(submit, line[1]);
@@ -178,128 +174,6 @@ void judge(char** argv) {
         }
         std::cout << '\r' << "[Judge] " << qn << "                    " << std::flush;
     } while (parse(output, line[0]) && parse(submit, line[1]));
-
-    // while (std::getline(submit, str_i)) {
-    //     parse(str_i, line[0]);
-    //     std::getline(output, str_i);
-    //     parse(str_i, line[1]);
-    //     while (line)
-    //     std::string qry = line[9];
-    //     std::string chr = line[2];
-    //     std::int64_t pos = std::stol(line[3]);
-    //     int r = std::find(chr_n.begin(), chr_n.end(), chr)-chr_n.begin();
-    //     if (r<chr_n.size())
-    //         pos = pos + (r?chr_c[r-1]:0) - 1;
-    // }
-
-    // int qn{};
-    // int err{};
-    // std::string qry;
-    // std::string chr;
-    // std::int64_t pos{};
-    // std::string rf;
-    // std::string aln;
-    // std::vector<int> aln_i;
-    // std::vector<char> aln_c;
-    // int val_m{}, val_p{}, val_d{}, tmp_d{};
-    // int qs{}, rs{};
-    // input >> qn;
-    // for (int qi=0; qi<qn; ++qi) {
-    //     for (int q=0; q<2; ++q) {
-    //         input >> qry;
-    //         submit >> chr >> pos >> rf >> aln;
-    //         if (rf=="R")
-    //             qry = rcseq(qry);
-    //         pos = pos + ((std::find(chr_n.begin(), chr_n.end(), chr)-chr_n.begin())*len/10) - 1;
-    //         aln_i.clear();
-    //         aln_c.clear();
-    //         for (int i=0; i<aln.size(); ++i) {
-    //             if (aln[i]>='A' && aln[i]<='Z') {
-    //                 aln_i.push_back(0);
-    //                 aln_c.push_back(aln[i]);
-    //             }
-    //             else
-    //                 aln_i.back() = aln_i.back() * 10 + (aln[i]-'0');
-    //         }
-    //         val_m = 0;
-    //         val_p = pos;
-    //         val_d = 0;
-    //         qs = 0;
-    //         rs = pos;
-    //         for (int i=0; i<aln_i.size(); ++i) {
-    //             if (aln_c[i]=='M') {
-    //                 for (int j=0; j<aln_i[i]; ++j) {
-    //                     if (seq[rs]!=qry[qs]) {
-    //                         std::cout << "aln: " << i << ' ' << j << ' ' << aln_i[i] << ' ' << aln_c[i] << '\n';
-    //                         std::cout << "M: " << rs-pos << seq[rs] << ' ' << qs << qry[qs] << '\n';
-    //                         val_m = 1;
-    //                         break;
-    //                     }
-    //                     qs += 1;
-    //                     rs += 1;
-    //                 }
-    //             }
-    //             else if (aln_c[i]=='S') {
-    //                 for (int j=0; j<aln_i[i]; ++j) {
-    //                     if (seq[rs]==qry[qs]) {
-    //                         std::cout << "S: " << rs-pos << seq[rs] << ' ' << qs << qry[qs] << '\n';
-    //                         val_m = 1;
-    //                         break;
-    //                     }
-    //                     qs += 1;
-    //                     rs += 1;
-    //                     val_d += 1;
-    //                 }
-    //             }
-    //             else if (aln_c[i]=='D') {
-    //                 for (int j=0; j<aln_i[i]; ++j) {
-    //                     rs += 1;
-    //                     val_d += 1;
-    //                 }
-    //             }
-    //             else {
-    //                 for (int j=0; j<aln_i[i]; ++j) {
-    //                     qs += 1;
-    //                     val_d += 1;
-    //                 }
-    //             }
-    //             if (val_m)
-    //                 break;
-    //         }
-    //         if (val_m) {
-    //             std::cout << "Match error: " << qi << "/" << q << '\n';
-    //             err += 1;
-    //             continue;
-    //         }
-    //         output >> chr >> pos >> rf >> aln;
-    //         pos = pos + ((std::find(chr_n.begin(), chr_n.end(), chr)-chr_n.begin())*len/10) - 1;
-    //         if ((val_p-pos)>10 || (val_p-pos)<-10) {
-    //             std::cout << "Position error: " << qi << "/" << q << '\n';
-    //             err += 1;
-    //             continue;
-    //         }
-    //         aln_i.clear();
-    //         aln_c.clear();
-    //         for (int i=0; i<aln.size(); ++i) {
-    //             if (aln[i]>='A' && aln[i]<='Z') {
-    //                 aln_i.push_back(0);
-    //                 aln_c.push_back(aln[i]);
-    //             }
-    //             else
-    //                 aln_i.back() = aln_i.back() * 10 + (aln[i]-'0');
-    //         }
-    //         tmp_d = 0;
-    //         for (int i=0; i<aln_i.size(); ++i) {
-    //             if (aln_c[i]!='M')
-    //                 tmp_d += aln_i[i];
-    //         }
-    //         if (val_d>tmp_d) {
-    //             std::cout << "Distance error: " << qi << "/" << q << '\n';
-    //             err += 1;
-    //         }
-    //     }
-    // }
-    // std::cout << "Error rate: " << err << "/" << qn << '\n';
     std::cout << '\r' << "[Judge] " << "Complete" << "                    \n" << std::flush; 
     std::cout << '\r' << "[Result] " << "                    \n" << std::flush;
     std::cout << '\r' << "    Mapping error: " << err[0] << '/' << qn << '\n';
