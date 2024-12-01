6b2c25fede4bd5afc4807e019b0b12bdbead22e7
hikarimusic <hikarimusic.tm@gmail.com>
2024-12-01 23:40:48 +0800

    cluster

--- README.md
+++ README.md
@@ -3,59 +3,143 @@ Automated Mapping And Transcriptome Expression Research Analysis Suite
 
 ## Setup
 
-Compile the files:
+Clone the repository:
 ```sh
-make
+git clone https://github.com/hikarimusic/AMATERASU.git
 ```
 
-Install Python packages:
+Make a virtual environment:
 ```sh
+python3 -m venv .amaterasu
+source .amaterasu/bin/activate
+```
+
+Install python packages:
+```sh
+cd AMATERASU/
 pip install -r requirements.txt
+```
+
+Download and index genome files with STAR:
+```sh
+chmod +x setup_STAR.sh process_STAR.sh
+./setup_STAR.sh
 ```
 
 ## Raw Data Processing
 
-Index the reference genome:
+Start AMATERASU:
 ```sh
-./index <GRCh38.fna>
+source .amaterasu/bin/activate
+cd AMATERASU/
 ```
 
-Align the sequences:
-```sh
-./align <GRCh38.fna> <input1.fq> <input2.fq> <output.sam>
-```
-
-Profile the mRNA expression:
-```sh
-./profile <gencode.gtf> <output.sam> <output.tsv>
-```
-
----
-If you want to use STAR to process the data :
-```sh
-chmod +x setup_STAR.sh process_STAR.sh
-```
-Download and index genome files:
-```sh
-./setup_STAR.sh
-```
-Align and profile RNA-seq data:
+Align and profile RNA-seq data with STAR:
 ```sh
 ./process_STAR.sh <input_dir/> <output_dir/>
 ```
 
+The `<input_dir/>` should contains sequencing raw data. Both pair-end and single-end data are acceptable. Example of `<input_dir/>`:
 
-## Expression Analysis
+```sh
+my_sequences/
+├── sample1_1.fastq # paired-end data
+├── sample1_2.fastq
+├── sample2.fastq # single-end data
+└── sample3/
+    ├── sample3_1.fastq # Files can be in subfolders
+    └── sample3_2.fastq
+```
 
-Summarize the profiles:
+The `<output_dir/>` will contain expression profiles of the samples. Example of `<output_dir/>`:
+```sh
+my_profiles/
+├── sample1.tsv
+├── sample2.tsv
+└── sample3.tsv
+```
+
+<!-- ```sh
+make
+./index <GRCh38.fna>
+./align <GRCh38.fna> <input1.fq> <input2.fq> <output.sam>
+./profile <gencode.gtf> <output.sam> <output.tsv>
+``` -->
+
+## Cohort Summary
+
+Start AMATERASU:
+```sh
+source .amaterasu/bin/activate
+cd AMATERASU/
+```
+
+Summarize the profiles based on you cohort:
 ```sh
 python3 summarize.py <cohort.csv> <profile_dir/> <value_type>
+```
+
+The first column of the cohort should be the sample ID. Example of `<cohort.csv>` (or `<cohort.tsv>`):
+
+| sample_id | gender | race | tumor_stage | 
+| :- | :- | :- | :- |
+| TCGA_DD_AAVP | male | asian | Stage I |
+| TCGA_WX_AA46 | male | white | Stage II |
+| TCGA_BD_A3EP | female | black | Stage I |
+
+The `<profile_dir/>` should contain profiles of those samples:
+
+```sh
+my_profiles/
+├── TCGA_DD_AAVP.tsv
+├── TCGA_WX_AA46.tsv
+└── TCGA_BD_A3EP.tsv
+```
+
+The `<value_type>` specifies which expression value to be used. Example of a profile tsv file (in this case we can use `tpm_unstranded` as the value type):
+```
+# gene-model: GENCODE v36
+gene_id	gene_name	gene_type	unstranded	stranded_first	stranded_second	tpm_unstranded	fpkm_unstranded	fpkm_uq_unstranded
+N_unmapped			8120268	8120268	8120268			
+N_multimapping			4402480	4402480	4402480			
+N_noFeature			1446120	24573336	24781939			
+N_ambiguous			5551010	1476463	1462076			
+ENSG00000000003.15	TSPAN6	protein_coding	3231	1634	1597	44.6685	16.8557	23.8956
+ENSG00000000005.6	TNMD	protein_coding	0	0	0	0.0000	0.0000	0.0000
+ENSG00000000419.13	DPM1	protein_coding	1083	556	528	56.2676	21.2327	30.1006
+```
+
+After running the command, a folder `<cohort/>` will be created. The file `summary.csv` will add gene expression values to the original cohort.
+
+## Clustering Analysis
+
+Start AMATERASU:
+```sh
+source .amaterasu/bin/activate
+cd AMATERASU/
 ```
 
 Perform clustering analysis:
 ```sh
 python3 cluster.py <cohort/summary.csv> <group_column1> <group_column2> ... 
 ```
+
+PCA plots corresponding to each column will be generated. Example:
+<img src="https://github.com/hikarimusic/AMATERASU/raw/main/assets/pca_plot.png" width=500>
+
+Heatmap with hierarchy clustering will be generated. Example:
+<img src="https://github.com/hikarimusic/AMATERASU/raw/main/assets/heatmap.png" width=500>
+
+If `[-n]` is specified, samples will be labeled into n clusters and new summary files will be generated. You can use `summary_cluster.csv` instead of `summary.csv` in the following analysis to access the cluster column.
+
+All the configuration such as figure sizes can be set in the head of `cluster.py`.
+
+## Temp
+
+
+
+
+
 
 Perform differentially expression analysis:
 ```sh
--- cluster.py
+++ cluster.py
@@ -1,3 +1,30 @@
+# -------------------------
+
+pca_plot_size = (3.5, 3.5)
+pca_plot_color = 'deep'
+pca_plot_dotsize = 10
+pca_plot_fontsize = 6
+pca_plot_format = 'png'
+pca_plot_labels = False
+pca_plot_group_order = {} # Example: {"Gender": ['female', 'male']}
+
+gene_threshold = 1
+gene_normalize = True
+
+cluster_gene_metric = 'correlation'
+cluster_case_metric = 'seuclidean'
+cluster_gene_method = 'ward'
+cluster_case_method = 'ward'
+
+heatmap_size = (7.0, 7.0)
+heatmap_color = 'seismic'
+heatmap_group_preset = ["Set1", "tab10", "Dark2"]
+heatmap_font_size = 6
+heatmap_format = 'png'
+heatmap_group_order = {} # Example: {"Gender": ['female', 'male']}
+
+# -------------------------
+
 import sys
 import os
 import pandas as pd
@@ -27,28 +54,26 @@ def generate_pca_plot(metadata, gene_data, group_columns, output_dir):
     # Create plots
     plt.style.use('ggplot')
     for group_column in group_columns:
-        plt.figure(figsize=(2.8, 2.8))
-        sns.scatterplot(data=pca_df, x='PC1', y='PC2', hue=group_column, palette='deep', s=10)
+        plt.figure(figsize=pca_plot_size)
+        if group_column in pca_plot_group_order:
+            hue_order = pca_plot_group_order[group_column]
+        else:
+            hue_order = sorted(metadata[group_column].unique(), key=str)
+        sns.scatterplot(data=pca_df, x='PC1', y='PC2', hue=group_column, palette=pca_plot_color, s=pca_plot_dotsize, hue_order=hue_order)
        
-        plt.xlabel(f'PC1 ({pca.explained_variance_ratio_[0]:.2%} variance)', fontsize=6)
-        plt.ylabel(f'PC2 ({pca.explained_variance_ratio_[1]:.2%} variance)', fontsize=6)
-        plt.xticks(fontsize=6)
-        plt.yticks(fontsize=6)
-        plt.legend(title=group_column, title_fontsize=5, fontsize=5)
-       
-        output_file = os.path.join(output_dir, f'cluster_PCA_{group_column.replace(" ", "_")}.pdf')
-        plt.savefig(output_file, format='pdf', dpi=600, bbox_inches='tight')
-        output_file = os.path.join(output_dir, f'cluster_PCA_{group_column.replace(" ", "_")}.png')
-        plt.savefig(output_file, format='png', dpi=600, bbox_inches='tight')
+        plt.xlabel(f'PC1 ({pca.explained_variance_ratio_[0]:.2%} variance)', fontsize=pca_plot_fontsize)
+        plt.ylabel(f'PC2 ({pca.explained_variance_ratio_[1]:.2%} variance)', fontsize=pca_plot_fontsize)
+        plt.xticks(fontsize=pca_plot_fontsize)
+        plt.yticks(fontsize=pca_plot_fontsize)
+        plt.legend(title=group_column, title_fontsize=pca_plot_fontsize, fontsize=pca_plot_fontsize)
 
         # Add sample ID labels
-        for idx, row in pca_df.iterrows():
-            plt.annotate(row[sample_ids.name], (row['PC1'], row['PC2']), xytext=(3, 3), textcoords='offset points', fontsize=5, alpha=0.7)
+        if pca_plot_labels == True:
+            for idx, row in pca_df.iterrows():
+                plt.annotate(row[sample_ids.name], (row['PC1'], row['PC2']), xytext=(3, 3), textcoords='offset points', fontsize=5, alpha=0.7)
 
-        output_file = os.path.join(output_dir, f'cluster_PCAs_{group_column.replace(" ", "_")}.pdf')
-        plt.savefig(output_file, format='pdf', dpi=600, bbox_inches='tight')
-        output_file = os.path.join(output_dir, f'cluster_PCAs_{group_column.replace(" ", "_")}.png')
-        plt.savefig(output_file, format='png', dpi=600, bbox_inches='tight')
+        output_file = os.path.join(output_dir, f'cluster_PCA_{group_column.replace(" ", "_")}.' + pca_plot_format)
+        plt.savefig(output_file, format=pca_plot_format, dpi=600, bbox_inches='tight')
         plt.close()
    
     print(f"[PCA] Complete                 ")
@@ -56,40 +81,63 @@ def generate_pca_plot(metadata, gene_data, group_columns, output_dir):
 def cluster(summarize_file, group_columns):
     # Read data
     print(f"[Read Data] ...", end='\r')  
-    df = pd.read_csv(summarize_file)
+    df = pd.read_csv(summarize_file, low_memory=False)
+    df = df.fillna('~')
     start_gene_index = df.columns.get_loc('START_GENE')
     metadata = df.iloc[:, :start_gene_index + 1]
     gene_data = df.iloc[:, start_gene_index + 1:]
     print("[Read Data] Complete                 ")
 
-    # PCA (before normalization)
+    # Remove cluster number
+    n_clusters = None
+    for column in group_columns:
+        if column.startswith('-'):
+            n_clusters = int(column[1:])
+            group_columns.remove(column)
+
+    # PCA
     generate_pca_plot(metadata, gene_data, group_columns, os.path.dirname(summarize_file))
 
     # Filter and normalize genes
     print(f"[Filter Genes] ...", end='\r')
-    expressed_genes = gene_data.columns[gene_data.mean() > 1]
+    expressed_genes = gene_data.columns[gene_data.mean() > gene_threshold]
     gene_data = gene_data[expressed_genes]
     high_var_genes = gene_data.columns[gene_data.var() > 0]
     selected_gene_data = gene_data[high_var_genes]
     target_median = selected_gene_data.median(axis=1).median(axis=0)
     scale_factors = target_median / selected_gene_data.median(axis=1)
+    if gene_normalize == False:
+        scale_factors = 1
     expression_data = selected_gene_data.multiply(scale_factors, axis=0)
     print(f"[Filter Genes] {expression_data.shape[1]}                 ")
 
     # Hierarchy clustering
     print(f"[Hierarchy Cluster] ...", end='\r')
-    gene_dist = pdist(expression_data.T, metric='correlation')
-    case_dist = pdist(expression_data, metric='seuclidean')
+    gene_dist = pdist(expression_data.T, metric=cluster_gene_metric)
+    case_dist = pdist(expression_data, metric=cluster_case_metric)
     
-    gene_linkage = hierarchy.linkage(gene_dist, method='ward')
-    case_linkage = hierarchy.linkage(case_dist, method='ward')
+    gene_linkage = hierarchy.linkage(gene_dist, method=cluster_gene_method)
+    case_linkage = hierarchy.linkage(case_dist, method=cluster_case_method)
     case_order = hierarchy.leaves_list(case_linkage)
     gene_order = hierarchy.leaves_list(gene_linkage)
     data_ordered = expression_data.iloc[case_order, gene_order]
 
-    cluster_labels = {}
-    for i in range(2, 11):
-        cluster_labels[i] = hierarchy.fcluster(case_linkage, t=i, criterion='maxclust')
+    # Output clustering results
+    if n_clusters is not None:
+        cluster_labels = hierarchy.fcluster(case_linkage, t=n_clusters, criterion='maxclust')
+        output_dir = os.path.dirname(summarize_file)
+        cohort_file = os.path.join(output_dir, 'cohort.csv')
+        cohort_df = pd.read_csv(cohort_file)
+        cohort_df['Cluster'] = [f"Cluster{label}" for label in cluster_labels]
+        clustered_cohort_output = os.path.join(output_dir, 'cohort_cluster.csv')
+        cohort_df.to_csv(clustered_cohort_output, index=False)
+        
+        summary_with_clusters = df.copy()
+        start_gene_idx = summary_with_clusters.columns.get_loc('START_GENE')
+        summary_with_clusters.insert(start_gene_idx, 'Cluster', [f"Cluster{label}" for label in cluster_labels])
+        clustered_summary_output = os.path.join(output_dir, 'summary_cluster.csv')
+        summary_with_clusters.to_csv(clustered_summary_output, index=False)
+
     print("[Hierarchy Cluster] Complete                 ")
     
     # Create heatmap
@@ -100,7 +148,7 @@ def cluster(summarize_file, group_columns):
                             index=data_ordered.index)
     
     plt.style.use('seaborn-v0_8-whitegrid')
-    fig = plt.figure(figsize=((12.5) * 0.45, (11 + 0.1 * len(group_columns)) * 0.45))
+    fig = plt.figure(figsize=heatmap_size)
     gs = fig.add_gridspec(2 + len(group_columns), 3, 
                           width_ratios=[1, 10, 1.5], 
                           height_ratios=[1] + [0.1] * len(group_columns) + [10],
@@ -120,17 +168,21 @@ def cluster(summarize_file, group_columns):
     ax_heatmap = fig.add_subplot(gs[1+len(group_columns), 1])
     vmin = np.percentile(z_scores.values, 1)
     vmax = np.percentile(z_scores.values, 99)
-    sns.heatmap(z_scores.T, cmap='seismic', center=0, vmin=vmin, vmax=vmax,
+    sns.heatmap(z_scores.T, cmap=heatmap_color, center=0, vmin=vmin, vmax=vmax,
                 xticklabels=False, yticklabels=False, cbar=False, ax=ax_heatmap)
     
     # Group indicators
-    color_preset = ["Set1", "tab10", "Dark2"]
+    color_preset = heatmap_group_preset
+    n_preset = len(color_preset)
     for i, group_column in enumerate(group_columns):
         ax_groups = fig.add_subplot(gs[1+i, 1], sharex=ax_heatmap)
         ax_groups.set_ylim(0, 1)
         
-        unique_groups = metadata[group_column].unique()
-        color_palette = sns.color_palette(color_preset[i%3], n_colors=len(unique_groups))
+        if group_column in heatmap_group_order:
+            unique_groups = heatmap_group_order[group_column]
+        else:
+            unique_groups = sorted(metadata[group_column].unique(), key=str)
+        color_palette = sns.color_palette(color_preset[i%n_preset], n_colors=len(unique_groups))
         color_map = dict(zip(unique_groups, color_palette))
         
         for j, sample in enumerate(data_ordered.index):
@@ -145,14 +197,17 @@ def cluster(summarize_file, group_columns):
    
     legend_elements = []
     for i, group_column in enumerate(group_columns):
-        unique_groups = metadata[group_column].unique()
-        color_palette = sns.color_palette(color_preset[i%3], n_colors=len(unique_groups))
+        if group_column in heatmap_group_order:
+            unique_groups = heatmap_group_order[group_column]
+        else:
+            unique_groups = sorted(metadata[group_column].unique(), key=str)
+        color_palette = sns.color_palette(color_preset[i%n_preset], n_colors=len(unique_groups))
         color_map = dict(zip(unique_groups, color_palette))
         legend_elements.extend([Rectangle((0, 0), 0.5, 0.5, facecolor="white", label=group_column)])
         legend_elements.extend([Rectangle((0, 0), 0.5, 0.5, facecolor=color_map[group], label=group) for group in unique_groups])
         legend_elements.extend([Rectangle((0, 0), 0.5, 0.5, facecolor="white", label="") for _ in range(4)])
 
-    cmap = plt.get_cmap('seismic')
+    cmap = plt.get_cmap(heatmap_color)
     z_min = round(np.floor(vmin / 0.2))
     z_max = round(np.ceil(vmax / 0.2))
     z_values = list(range(z_max, z_min, -1))
@@ -168,7 +223,7 @@ def cluster(summarize_file, group_columns):
     legend = ax_legend.legend(handles=all_elements, loc='center', 
                               ncol=1, handlelength=1, handleheight=1, 
                               handletextpad=0.5, columnspacing=0.5, labelspacing=0.0,
-                              prop={'size': 5})
+                              prop={'size': heatmap_font_size})
 
     for text in legend.get_texts():
         if text.get_text() in group_columns:
@@ -180,20 +235,10 @@ def cluster(summarize_file, group_columns):
     # Save the plot
     print("[Save Heatmap] ...", end='\r')
     output_dir = os.path.dirname(summarize_file)
-    # output_file = os.path.join(output_dir, f'Cluster_heatmap_{"_".join(group_columns).replace(" ", "_")}.pdf')
-    # plt.savefig(output_file, format='pdf', dpi=600, bbox_inches='tight')
-    output_file = os.path.join(output_dir, f'cluster_heatmap_{"_".join(group_columns).replace(" ", "_")}.png')
-    plt.savefig(output_file, format='png', dpi=600, bbox_inches='tight')
+    output_file = os.path.join(output_dir, f'cluster_heatmap_{"_".join(group_columns).replace(" ", "_")}.' + heatmap_format)
+    plt.savefig(output_file, format=heatmap_format, dpi=600, bbox_inches='tight')
     plt.close()
     
-    # Output clustering.csv
-    cohort_file = os.path.join(output_dir, 'cohort.csv')
-    cohort_df = pd.read_csv(cohort_file)
-    cohort_df_ordered = cohort_df.loc[data_ordered.index]
-    for i in range(2, 11):
-        cohort_df_ordered[f'{i}_clusters'] = [f"{i}_cluster{label}" for label in cluster_labels[i][case_order]]
-    clustered_output = os.path.join(output_dir, 'cohort_clustered.csv')
-    cohort_df_ordered.to_csv(clustered_output, index=False)
     print("[Save Heatmap] Complete                 ")
 
 if __name__ == "__main__":
